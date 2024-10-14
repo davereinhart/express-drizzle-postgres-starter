815e92ebbe38262babae8a9eadb03b24149d78f6
David Reinhart <davidrr@uw.edu>
2024-10-14 11:10:12 -0700

    Add JSON Schema to specimens GET response

--- src/controllers/specimen-controllers.ts
+++ src/controllers/specimen-controllers.ts
@@ -1,11 +1,13 @@
 import {
   getAllSpecimens,
+  getSpecimensJsonSchema,
 } from '@/services/specimen-services'
 import { createHandler } from '@/utils/create'
 
 export const handleGetAllSpecimens = createHandler(async (_req, res) => {
   const specimens = await getAllSpecimens()
   res.status(200).json({
-    specimens,
+    records: specimens,
+    schema: getSpecimensJsonSchema(),
   })
 })
--- src/services/specimen-services.ts
+++ src/services/specimen-services.ts
@@ -1,10 +1,12 @@
-import { specimens } from '@/schema/specimen'
+import { selectSpecimenSchema, specimens } from '@/db/schema/specimen'
 import { db } from '@/utils/db'
 import { and, eq } from 'drizzle-orm'
+import { zodToJsonSchema } from 'zod-to-json-schema'
 
 export async function getAllSpecimens() {
-  return await db.select({
-    id: specimens.id,
-    name: specimens.name,
-  }).from(specimens)
+  return await db.select().from(specimens)
+}
+
+export function getSpecimensJsonSchema() {
+  return zodToJsonSchema(selectSpecimenSchema)
 }
