0674161ff8c69b551f9c7d5e616c4fe3cb05f221
David Reinhart <davidrr@uw.edu>
2024-10-15 09:11:02 -0700

    Adding specimen update

--- src/controllers/specimen-controllers.ts
+++ src/controllers/specimen-controllers.ts
@@ -1,13 +1,29 @@
 import {
+  type Specimen,
+  updateSpecimenSchema,
+} from '@/db/schema/specimen'
+import {
   getAllSpecimens,
   getSpecimensJsonSchema,
+  updateSpecimen,
 } from '@/services/specimen-services'
 import { createHandler } from '@/utils/create'
+import consola from 'consola'
 
 export const handleGetAllSpecimens = createHandler(async (_req, res) => {
   const specimens = await getAllSpecimens()
   res.status(200).json({
     records: specimens,
     schema: getSpecimensJsonSchema(),
+  })
+})
+
+export const handleUpdateSpecimen = createHandler(updateSpecimenSchema, async (req, res) => {
+  const { id, name } = req.body
+
+  const updatedSpecimen = await updateSpecimen(id, { name })
+
+  res.status(200).json({
+    record: updatedSpecimen,
   })
 })
--- src/services/specimen-services.ts
+++ src/services/specimen-services.ts
@@ -1,10 +1,30 @@
-import { selectSpecimenSchema, specimens } from '@/db/schema/specimen'
+import { selectSpecimenSchema, type Specimen, specimens, type UpdateSpecimen } from '@/db/schema/specimen'
 import { db } from '@/utils/db'
 import { and, eq } from 'drizzle-orm'
 import { zodToJsonSchema } from 'zod-to-json-schema'
 
 export async function getAllSpecimens() {
   return await db.select().from(specimens)
+}
+
+export async function updateSpecimen(specimenId: string, { name }: UpdateSpecimen) {
+  const [updatedSpecimen] = await db
+    .update(specimens)
+    .set({
+      name,
+    })
+    .where(eq(specimens.id, specimenId))
+    .returning(
+      {
+        id: specimens.id,
+        name: specimens.name,
+        updated_at: specimens.updated_at,
+        created_at: specimens.created_at,
+        deleted_at: specimens.deleted_at,
+      },
+    )
+
+  return updatedSpecimen
 }
 
 export function getSpecimensJsonSchema() {
